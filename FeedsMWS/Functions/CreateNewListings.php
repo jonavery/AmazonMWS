8ee17ec0bb540fa6ddbebc864ff8acdd61a6b972
Jon <jon.avery92@gmail.com>
2017-05-24 07:58:52 -0700

    Parse Google Drive instead of accessing other php file

--- FeedsMWS/Functions/CreateNewListings.php
+++ FeedsMWS/Functions/CreateNewListings.php
@@ -15,8 +15,31 @@
  * processing.
  *******************************************************************/
 
-require_once $_SERVER['DOCUMENT_ROOT']."/AmazonMWS/MarketplaceWebServiceProducts/CreateItemArray.php";
+// @TODO: Create new tab in Work that includes information
+// @TODO: obtained through the Products API and load that instead.
+// Load XML file.
+$url = "https://script.google.com/macros/s/AKfycbxoNDu7BM4PRE1DEDVyCTd5lkMK1cGPLV0C8KujXDgc3CKNqljU/exec";
 
+// Parse data from XML into an array.
+$itemsXML = file_get_contents($url);
+$items = new SimpleXMLElement($itemsXML);
+$itemArray = array();
+foreach ($items->item as $item) {
+    if ((string)$item->Title == "") {
+	continue;
+    }
+    $itemArray[] = array(
+	"SellerSKU"=>(string)$item->SKU,
+	"Title"=>(string)$item->Title,
+	"UPC"=>(string)$item->ASIN,
+	"Condition"=>(string)$item->Condition,
+	"Comment"=>(string)$item->Comment,
+	"Dimensions"=>array(
+	    "Weight"=>(string)$item->Dimensions->Weight,
+	    "Length"=>(string)$item->Dimensions->Length,
+	    "Width"=>(string)$item->Dimensions->Width,
+	    "Height"=>(string)$item->Dimensions->Height));
+}
 
 
 
