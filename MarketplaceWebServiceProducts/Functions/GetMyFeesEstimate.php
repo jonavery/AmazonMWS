6d4df18b70695d70b15dd61170d896b98adebb26
Jon <jon.avery92@gmail.com>
2017-06-19 17:21:21 -0700

    Update GetMyFeesEstimate to Klasrun standards

--- MarketplaceWebServiceProducts/Functions/GetMyFeesEstimate.php
+++ MarketplaceWebServiceProducts/Functions/GetMyFeesEstimate.php
@@ -18,7 +18,7 @@
  */
 
 /**
- * Get My Fees Estimate Sample
+ * Get My Fees Estimate
  */
 
 require_once('.config.inc.php');
@@ -32,52 +32,32 @@ require_once('.config.inc.php');
  ***********************************************************************/
 // More endpoints are listed in the MWS Developer Guide
 // North America:
-//$serviceUrl = "https://mws.amazonservices.com/Products/2011-10-01";
-// Europe
-//$serviceUrl = "https://mws-eu.amazonservices.com/Products/2011-10-01";
-// Japan
-//$serviceUrl = "https://mws.amazonservices.jp/Products/2011-10-01";
-// China
-//$serviceUrl = "https://mws.amazonservices.com.cn/Products/2011-10-01";
+$serviceUrl = "https://mws.amazonservices.com/Products/2011-10-01";
 
+$config = array (
+  'ServiceURL' => $serviceUrl,
+  'ProxyHost' => null,
+  'ProxyPort' => -1,
+  'ProxyUsername' => null,
+  'ProxyPassword' => null,
+  'MaxErrorRetry' => 3,
+);
 
- $config = array (
-   'ServiceURL' => $serviceUrl,
-   'ProxyHost' => null,
-   'ProxyPort' => -1,
-   'ProxyUsername' => null,
-   'ProxyPassword' => null,
-   'MaxErrorRetry' => 3,
- );
+$service = new MarketplaceWebServiceProducts_Client(
+       AWS_ACCESS_KEY_ID,
+       AWS_SECRET_ACCESS_KEY,
+       APPLICATION_NAME,
+       APPLICATION_VERSION,
+       $config);
 
- $service = new MarketplaceWebServiceProducts_Client(
-        AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY,
-        APPLICATION_NAME,
-        APPLICATION_VERSION,
-        $config);
-
-/************************************************************************
- * Uncomment to try out Mock Service that simulates MarketplaceWebServiceProducts
- * responses without calling MarketplaceWebServiceProducts service.
- *
- * Responses are loaded from local XML files. You can tweak XML files to
- * experiment with various outputs during development
- *
- * XML files available under MarketplaceWebServiceProducts/Mock tree
- *
- ***********************************************************************/
- // $service = new MarketplaceWebServiceProducts_Mock();
 
 /************************************************************************
  * Setup request parameters and uncomment invoke to try out
  * sample for Get My Fees Estimate Action
  ***********************************************************************/
- // @TODO: set request. Action can be passed as MarketplaceWebServiceProducts_Model_GetMyFeesEstimate
- $request = new MarketplaceWebServiceProducts_Model_GetMyFeesEstimateRequest();
- $request->setSellerId(MERCHANT_ID);
- // object or array of parameters
- invokeGetMyFeesEstimate($service, $request);
+$request = new MarketplaceWebServiceProducts_Model_GetMyFeesEstimateRequest();
+$request->setSellerId(MERCHANT_ID);
+// object or array of parameters
 
 /**
   * Get Get My Fees Estimate Action Sample
@@ -88,29 +68,26 @@ require_once('.config.inc.php');
   * @param mixed $request MarketplaceWebServiceProducts_Model_GetMyFeesEstimate or array of parameters
   */
 
-  function invokeGetMyFeesEstimate(MarketplaceWebServiceProducts_Interface $service, $request)
-  {
-      try {
-        $response = $service->GetMyFeesEstimate($request);
+ function invokeGetMyFeesEstimate(MarketplaceWebServiceProducts_Interface $service, $request)
+ {
+     try {
+       $response = $service->GetMyFeesEstimate($request);
 
-        echo ("Service Response\n");
-        echo ("=============================================================================\n");
+       $dom = new DOMDocument();
+       $dom->loadXML($response->toXML());
+       $dom->preserveWhiteSpace = false;
+       $dom->formatOutput = true;
+       $response->getResponseHeaderMetadata();
+       return $dom->saveXML();
 
-        $dom = new DOMDocument();
-        $dom->loadXML($response->toXML());
-        $dom->preserveWhiteSpace = false;
-        $dom->formatOutput = true;
-        echo $dom->saveXML();
-        echo("ResponseHeaderMetadata: " . $response->getResponseHeaderMetadata() . "\n");
-
-     } catch (MarketplaceWebServiceProducts_Exception $ex) {
-        echo("Caught Exception: " . $ex->getMessage() . "\n");
-        echo("Response Status Code: " . $ex->getStatusCode() . "\n");
-        echo("Error Code: " . $ex->getErrorCode() . "\n");
-        echo("Error Type: " . $ex->getErrorType() . "\n");
-        echo("Request ID: " . $ex->getRequestId() . "\n");
-        echo("XML: " . $ex->getXML() . "\n");
-        echo("ResponseHeaderMetadata: " . $ex->getResponseHeaderMetadata() . "\n");
-     }
- }
+    } catch (MarketplaceWebServiceProducts_Exception $ex) {
+       echo("Caught Exception: " . $ex->getMessage() . "\n");
+       echo("Response Status Code: " . $ex->getStatusCode() . "\n");
+       echo("Error Code: " . $ex->getErrorCode() . "\n");
+       echo("Error Type: " . $ex->getErrorType() . "\n");
+       echo("Request ID: " . $ex->getRequestId() . "\n");
+       echo("XML: " . $ex->getXML() . "\n");
+       echo("ResponseHeaderMetadata: " . $ex->getResponseHeaderMetadata() . "\n");
+    }
+}
 
