21d16613c56fb936e27fc120d5fbb36949d9b621
Jon <jon.avery92@gmail.com>
2017-06-21 09:22:05 -0700

    Add Subscription API

---
+++ MWSSubscriptionsService/Functions/MasterSubscription.php
@@ -0,0 +1,7 @@
+<?php
+
+/*************************************************************************************
+ * This script utilizes the Subscriptions API to setup subscription feeds and have
+ * them sent to the Klasrun AWS SQS queue.
+ * *********************************************************************************/
+
