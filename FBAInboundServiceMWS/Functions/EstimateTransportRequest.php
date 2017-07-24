6c2d155e256984612ed8bfad7dc84264b9d3f342
Jon <jon.avery92@gmail.com>
2017-07-23 17:23:45 -0700

    Fix syntax error on line 24

--- FBAInboundServiceMWS/Functions/EstimateTransportRequest.php
+++ FBAInboundServiceMWS/Functions/EstimateTransportRequest.php
@@ -21,7 +21,7 @@
  * Estimate Transport Request Sample
  */
 
-require_once(__DIR__ '/.config.inc.php');
+require_once(__DIR__ . '/.config.inc.php');
 
 /************************************************************************
  * Instantiate Implementation of FBAInboundServiceMWS
