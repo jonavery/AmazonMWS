c8fafb30289db0c745ba6f868c7f8f6d76be6e27
Jon <jon.avery92@gmail.com>
2017-10-01 11:45:14 -0700

    Clean up syntax in function output

--- FBAInboundServiceMWS/Functions/GetInboundGuidanceForASIN.php
+++ FBAInboundServiceMWS/Functions/GetInboundGuidanceForASIN.php
@@ -88,29 +88,26 @@ require_once('.config.inc.php');
   * @param mixed $request FBAInboundServiceMWS_Model_GetInboundGuidanceForASIN or array of parameters
   */
 
-  function invokeGetInboundGuidanceForASIN(FBAInboundServiceMWS_Interface $service, $request)
-  {
-      try {
-        $response = $service->GetInboundGuidanceForASIN($request);
+ function invokeGetInboundGuidanceForASIN(FBAInboundServiceMWS_Interface $service, $request)
+ {
+     try {
+       $response = $service->GetInboundGuidanceForASIN($request);
 
-        echo ("Service Response\n");
-        echo ("=============================================================================\n");
+       $dom = new DOMDocument();
+       $dom->loadXML($response->toXML());
+       $dom->preserveWhiteSpace = false;
+       $dom->formatOutput = true;
+       $response->getResponseHeaderMetadata();
+       return $dom->saveXML();
 
-        $dom = new DOMDocument();
-        $dom->loadXML($response->toXML());
-        $dom->preserveWhiteSpace = false;
-        $dom->formatOutput = true;
-        echo $dom->saveXML();
-        echo("ResponseHeaderMetadata: " . $response->getResponseHeaderMetadata() . "\n");
-
-     } catch (FBAInboundServiceMWS_Exception $ex) {
-        echo("Caught Exception: " . $ex->getMessage() . "\n");
-        echo("Response Status Code: " . $ex->getStatusCode() . "\n");
-        echo("Error Code: " . $ex->getErrorCode() . "\n");
-        echo("Error Type: " . $ex->getErrorType() . "\n");
-        echo("Request ID: " . $ex->getRequestId() . "\n");
-        echo("XML: " . $ex->getXML() . "\n");
-        echo("ResponseHeaderMetadata: " . $ex->getResponseHeaderMetadata() . "\n");
-     }
- }
+    } catch (FBAInboundServiceMWS_Exception $ex) {
+       echo("Caught Exception: " . $ex->getMessage() . "\n");
+       echo("Response Status Code: " . $ex->getStatusCode() . "\n");
+       echo("Error Code: " . $ex->getErrorCode() . "\n");
+       echo("Error Type: " . $ex->getErrorType() . "\n");
+       echo("Request ID: " . $ex->getRequestId() . "\n");
+       echo("XML: " . $ex->getXML() . "\n");
+       echo("ResponseHeaderMetadata: " . $ex->getResponseHeaderMetadata() . "\n");
+    }
+}
 
