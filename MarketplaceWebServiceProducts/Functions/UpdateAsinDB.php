7400a7375c9e8b2423f3473195a02685f3a6825e
Jon <jon.avery92@gmail.com>
2017-10-06 14:57:15 -0700

    Call Google ASIN DB and cache as array

--- MarketplaceWebServiceProducts/Functions/UpdateAsinDB.php
+++ MarketplaceWebServiceProducts/Functions/UpdateAsinDB.php
@@ -6,10 +6,6 @@
  *
  **********************************************************/
 
-require_once('SetItemPrice.php');
-require_once('GetLowestOfferListingsForASIN.php');
-$requestPrice = $request;
-
 // Define database parameters.
 $host = "localhost";
 $db = "klasrunc_inventory";
@@ -38,11 +34,14 @@ $stmt = $db->prepare('
 $stmt->execute();
 $asinPDOS = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// @TODO: Call the Google DB and convert JSON to an array.
+// Call the Google DB and convert JSON to an array.
+$url = "https://script.google.com/macros/s/AKfycbwKZalsFAweoZtHoFEzmz-W505BN4P7sQ6zDP4HSI4AXK8Tsdw/exec";
+$asinJSON = file_get_contents($url);
+$asinArray = json_decode($asinJSON, true);
 
 echo "Comparing ASINs in MySQL to those in GoogleDB. \n";
 // Check each ASIN from the Google DB to see if it's in the prices table.
-foreach ($asinPDOS as $row) {
+foreach ($asinArray as $asinRow) {
     // If the ASIN is not in the prices table, insert it.
     // @TODO: Change query to an insert with the item to be added.
     $stmt = $db->prepare('UPDATE prices SET ListPrice = :price WHERE ASIN = :asin');
