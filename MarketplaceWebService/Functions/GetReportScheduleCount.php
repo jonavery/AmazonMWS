96bca287f3cdd494c724a4358a230be8ac904ac3
LeonUu <leon_204@hotmail.com>
2017-10-08 16:21:37 -0700

    GetReportScheduleCount.php ServiceUrl activated, Deleted comments: Mock Service, Client instance configuration, Sample request construction

--- MarketplaceWebService/Functions/GetReportScheduleCount.php
+++ MarketplaceWebService/Functions/GetReportScheduleCount.php
@@ -17,39 +17,15 @@
  */
 
 /**
- * Get Report Schedule Count  Sample
+ * Get Report Schedule Count  
  */
 
 include_once ('.config.inc.php'); 
 
-/************************************************************************
-* Uncomment to configure the client instance. Configuration settings
-* are:
-*
-* - MWS endpoint URL
-* - Proxy host and port.
-* - MaxErrorRetry.
-***********************************************************************/
 // IMPORTANT: Uncomment the approiate line for the country you wish to
 // sell in:
 // United States:
-//$serviceUrl = "https://mws.amazonservices.com";
-// United Kingdom
-//$serviceUrl = "https://mws.amazonservices.co.uk";
-// Germany
-//$serviceUrl = "https://mws.amazonservices.de";
-// France
-//$serviceUrl = "https://mws.amazonservices.fr";
-// Italy
-//$serviceUrl = "https://mws.amazonservices.it";
-// Japan
-//$serviceUrl = "https://mws.amazonservices.jp";
-// China
-//$serviceUrl = "https://mws.amazonservices.com.cn";
-// Canada
-//$serviceUrl = "https://mws.amazonservices.ca";
-// India
-//$serviceUrl = "https://mws.amazonservices.in";
+$serviceUrl = "https://mws.amazonservices.com";
 
 $config = array (
   'ServiceURL' => $serviceUrl,
@@ -72,41 +48,10 @@ $config = array (
      APPLICATION_NAME,
      APPLICATION_VERSION);
  
-/************************************************************************
- * Uncomment to try out Mock Service that simulates MarketplaceWebService
- * responses without calling MarketplaceWebService service.
- *
- * Responses are loaded from local XML files. You can tweak XML files to
- * experiment with various outputs during development
- *
- * XML files available under MarketplaceWebService/Mock tree
- *
- ***********************************************************************/
- // $service = new MarketplaceWebService_Mock();
-
-/************************************************************************
- * Setup request parameters and uncomment invoke to try out 
- * sample for Get Report Schedule Count Action
- ***********************************************************************/
- // @TODO: set request. Action can be passed as MarketplaceWebService_Model_GetReportScheduleCountRequest
- // object or array of parameters
- 
-//$parameters = array (
-//  'Merchant' => MERCHANT_ID,
-//  'MWSAuthToken' => '<MWS Auth Token>', // Optional
-//);
-//
-//$request = new MarketplaceWebService_Model_GetReportScheduleCountRequest($parameters);
-//
-//$request = new MarketplaceWebService_Model_GetReportScheduleCountRequest();
-//$request->setMerchant(MERCHANT_ID);
-//$request->setMWSAuthToken('<MWS Auth Token>'); // Optional
-//     
 //invokeGetReportScheduleCount($service, $request);
-
                             
 /**
-  * Get Report Schedule Count Action Sample
+  * Get Report Schedule Count Action 
   * returns the number of report schedules
   *   
   * @param MarketplaceWebService_Interface $service instance of MarketplaceWebService_Interface
