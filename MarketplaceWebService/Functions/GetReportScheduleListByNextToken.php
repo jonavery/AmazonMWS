3a70f7e06d1e91c41d1e42ed61c1cb55d25adea6
LeonUu <leon_204@hotmail.com>
2017-10-08 16:24:35 -0700

    GetReportScheduleListByNextToken.php ServiceUrl activated, Deleted comments: Mock Service, Client instance configuration, Sample request construction

--- MarketplaceWebService/Functions/GetReportScheduleListByNextToken.php
+++ MarketplaceWebService/Functions/GetReportScheduleListByNextToken.php
@@ -17,39 +17,15 @@
  */
 
 /**
- * Get Report Schedule List By Next Token  Sample
+ * Get Report Schedule List By Next Token  
  */
 
 include_once ('.config.inc.php'); 
 
-/************************************************************************
-* Uncomment to configure the client instance. Configuration settings
-* are:
-*
-* - MWS endpoint URL
-* - Proxy host and port.
-* - MaxErrorRetry.
-***********************************************************************/
 // IMPORTANT: Uncomment the approiate line for the country you wish to
 // sell in:
 // United States:
-//$serviceUrl = "https://mws.amazonservices.com";
-// United Kingdom
-//$serviceUrl = "https://mws.amazonservices.co.uk";
-// Germany
-//$serviceUrl = "https://mws.amazonservices.de";
-// France
-//$serviceUrl = "https://mws.amazonservices.fr";
-// Italy
-//$serviceUrl = "https://mws.amazonservices.it";
-// Japan
-//$serviceUrl = "https://mws.amazonservices.jp";
-// China
-//$serviceUrl = "https://mws.amazonservices.com.cn";
-// Canada
-//$serviceUrl = "https://mws.amazonservices.ca";
-// India
-//$serviceUrl = "https://mws.amazonservices.in";
+$serviceUrl = "https://mws.amazonservices.com";
 
 $config = array (
   'ServiceURL' => $serviceUrl,
@@ -68,47 +44,15 @@ $config = array (
  $service = new MarketplaceWebService_Client(
      AWS_ACCESS_KEY_ID, 
      AWS_SECRET_ACCESS_KEY, 
-     $config,
+     APPLICATION_VERSION,
      APPLICATION_NAME,
-     APPLICATION_VERSION);
- 
-/************************************************************************
- * Uncomment to try out Mock Service that simulates MarketplaceWebService
- * responses without calling MarketplaceWebService service.
- *
- * Responses are loaded from local XML files. You can tweak XML files to
- * experiment with various outputs during development
- *
- * XML files available under MarketplaceWebService/Mock tree
- *
- ***********************************************************************/
- // $service = new MarketplaceWebService_Mock();
+     $config);
 
-/************************************************************************
- * Setup request parameters and uncomment invoke to try out 
- * sample for Get Report Schedule List By Next Token Action
- ***********************************************************************/
- // @TODO: set request. Action can be passed as MarketplaceWebService_Model_GetReportScheduleListByNextTokenRequest
- // object or array of parameters
-
-//$parameters = array (
-//  'Merchant' => MERCHANT_ID,
-//  'NextToken' => '<Next Token Returned by GetReportScheduleList>',
-//  'MWSAuthToken' => '<MWS Auth Token>', // Optional
-//);
-//
-//$request = new MarketplaceWebService_Model_GetReportScheduleListByNextTokenRequest($parameters);
-//
-//$request = new MarketplaceWebService_Model_GetReportScheduleListByNextTokenRequest();
-//$request->setMerchant(MERCHANT_ID);
-//$request->setNextToken('<Next Token Returned by GetReportScheduleList>');
-//$request->setMWSAuthToken('<MWS Auth Token>'); // Optional
 //
 //invokeGetReportScheduleListByNextToken($service, $request);
-
                                                                                     
 /**
-  * Get Report Schedule List By Next Token Action Sample
+  * Get Report Schedule List By Next Token Action
   * retrieve the next batch of list items and if there are more items to retrieve
   *   
   * @param MarketplaceWebService_Interface $service instance of MarketplaceWebService_Interface
