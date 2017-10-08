6755af31549546d33f1374fea29ecc2a6fcffeed
LeonUu <leon_204@hotmail.com>
2017-10-08 16:25:59 -0700

    ManageReportSchedule.php ServiceUrl activated, Deleted comments: Mock Service, Client instance configuration, Sample request construction

--- MarketplaceWebService/Functions/ManageReportSchedule.php
+++ MarketplaceWebService/Functions/ManageReportSchedule.php
@@ -13,43 +13,20 @@
 
  *  Marketplace Web Service PHP5 Library
  *  Generated: Thu May 07 13:07:36 PDT 2009
- * 
  */
 
 /**
- * Manage Report Schedule  Sample
+ * Manage Report Schedule
+ * Creates, updates, or deletes a report schedule
+ * for a given report type, such as order reports in particular.
  */
 
 include_once ('.config.inc.php'); 
 
-/************************************************************************
-* Uncomment to configure the client instance. Configuration settings
-* are:
-*
-* - MWS endpoint URL
-* - Proxy host and port.
-* - MaxErrorRetry.
-***********************************************************************/
 // IMPORTANT: Uncomment the approiate line for the country you wish to
 // sell in:
 // United States:
-//$serviceUrl = "https://mws.amazonservices.com";
-// United Kingdom
-//$serviceUrl = "https://mws.amazonservices.co.uk";
-// Germany
-//$serviceUrl = "https://mws.amazonservices.de";
-// France
-//$serviceUrl = "https://mws.amazonservices.fr";
-// Italy
-//$serviceUrl = "https://mws.amazonservices.it";
-// Japan
-//$serviceUrl = "https://mws.amazonservices.jp";
-// China
-//$serviceUrl = "https://mws.amazonservices.com.cn";
-// Canada
-//$serviceUrl = "https://mws.amazonservices.ca";
-// India
-//$serviceUrl = "https://mws.amazonservices.in";
+$serviceUrl = "https://mws.amazonservices.com";
 
 $config = array (
   'ServiceURL' => $serviceUrl,
@@ -68,51 +45,14 @@ $config = array (
  $service = new MarketplaceWebService_Client(
      AWS_ACCESS_KEY_ID, 
      AWS_SECRET_ACCESS_KEY, 
-     $config,
+     APPLICATION_VERSION,
      APPLICATION_NAME,
-     APPLICATION_VERSION);
- 
-/************************************************************************
- * Uncomment to try out Mock Service that simulates MarketplaceWebService
- * responses without calling MarketplaceWebService service.
- *
- * Responses are loaded from local XML files. You can tweak XML files to
- * experiment with various outputs during development
- *
- * XML files available under MarketplaceWebService/Mock tree
- *
- ***********************************************************************/
- // $service = new MarketplaceWebService_Mock();
+     $config);
 
-/************************************************************************
- * Setup request parameters and uncomment invoke to try out 
- * sample for Manage Report Schedule Action
- ***********************************************************************/
- // @TODO: set request. Action can be passed as MarketplaceWebService_Model_ManageReportScheduleRequest
- // object or array of parameters
-
-//$parameters = array (
-//  'Merchant' => MERCHANT_ID,
-//  'ReportType' => '_GET_ORDERS_DATA_',
-//  'Schedule' => '_1_HOUR_',
-//  'ScheduleDate' => new DateTime('now', new DateTimeZone('UTC')),
-//  'MWSAuthToken' => '<MWS Auth Token>', // Optional
-//);
-//
-//$request = new MarketplaceWebService_Model_ManageReportScheduleRequest($parameters);
-
-//$request = new MarketplaceWebService_Model_ManageReportScheduleRequest();
-//$request->setMerchant(MERCHANT_ID);
-//$request->setReportType('_GET_ORDERS_DATA_');
-//$request->setSchedule('_1_HOUR_');
-//$request->setScheduleDate(new DateTime('now', new DateTimeZone('UTC')));
-//$request->setMWSAuthToken('<MWS Auth Token>'); // Optional
-//
 //invokeManageReportSchedule($service, $request);
-
                                                                                             
 /**
-  * Manage Report Schedule Action Sample
+  * Manage Report Schedule Action
   * Creates, updates, or deletes a report schedule
   * for a given report type, such as order reports in particular.
   *   
