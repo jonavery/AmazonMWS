b2f6b660990a7cdfba0b06711e0eb81a93bf0250
LeonUu <leon_204@hotmail.com>
2017-10-08 16:10:58 -0700

    GetReport.php ServiceUrl activated, Deleted comments: Mock Service, Client instance configuration, Sample request construction

--- MarketplaceWebService/Functions/GetReport.php
+++ MarketplaceWebService/Functions/GetReport.php
@@ -17,39 +17,15 @@
  */
 
 /**
- * Get Report  Sample
+ * Get Report  
  */
 
 include_once ('.config.inc.php'); 
 
-/************************************************************************
-* Uncomment to configure the client instance. Configuration settings
-* are:
-*
-* - MWS endpoint URL
-* - Proxy host and port.
-* - MaxErrorRetry.
-***********************************************************************/
 // IMPORTANT: Uncomment the approiate line for the country you wish to
 // sell in:
 // United States:
-//$serviceUrl = "https://mws.amazonservices.com";
-// United Kingdom
-//$serviceUrl = "https://mws.amazonservices.co.uk";
-// Germany
-//$serviceUrl = "https://mws.amazonservices.de";
-// France
-//$serviceUrl = "https://mws.amazonservices.fr";
-// Italy
-//$serviceUrl = "https://mws.amazonservices.it";
-// Japan
-//$serviceUrl = "https://mws.amazonservices.jp";
-// China
-//$serviceUrl = "https://mws.amazonservices.com.cn";
-// Canada
-//$serviceUrl = "https://mws.amazonservices.ca";
-// India
-//$serviceUrl = "https://mws.amazonservices.in";
+$serviceUrl = "https://mws.amazonservices.com";
 
 $config = array (
   'ServiceURL' => $serviceUrl,
@@ -71,45 +47,11 @@ $config = array (
      $config,
      APPLICATION_NAME,
      APPLICATION_VERSION);
- 
-/************************************************************************
- * Uncomment to try out Mock Service that simulates MarketplaceWebService
- * responses without calling MarketplaceWebService service.
- *
- * Responses are loaded from local XML files. You can tweak XML files to
- * experiment with various outputs during development
- *
- * XML files available under MarketplaceWebService/Mock tree
- *
- ***********************************************************************/
- // $service = new MarketplaceWebService_Mock();
-
-/************************************************************************
- * Setup request parameters and uncomment invoke to try out 
- * sample for Get Report Action
- ***********************************************************************/
- // @TODO: set request. Action can be passed as MarketplaceWebService_Model_GetReportRequest
- // object or array of parameters
-// $reportId = '<Your Report Id>';
- 
-// $parameters = array (
-//   'Merchant' => MERCHANT_ID,
-//   'Report' => @fopen('php://memory', 'rw+'),
-//   'ReportId' => $reportId,
-//   'MWSAuthToken' => '<MWS Auth Token>', // Optional
-// );
-// $request = new MarketplaceWebService_Model_GetReportRequest($parameters);
-
-//$request = new MarketplaceWebService_Model_GetReportRequest();
-//$request->setMerchant(MERCHANT_ID);
-//$request->setReport(@fopen('php://memory', 'rw+'));
-//$request->setReportId($reportId);
-//$request->setMWSAuthToken('<MWS Auth Token>'); // Optional
- 
+  
 invokeGetReport($service, $request);
 
 /**
-  * Get Report Action Sample
+  * Get Report Action
   * The GetReport operation returns the contents of a report. Reports can potentially be
   * very large (>100MB) which is why we only return one report at a time, and in a
   * streaming fashion.
