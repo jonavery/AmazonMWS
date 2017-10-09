4358b3ea281efdac27dae149e2fe6c2849448088
LeonUu <leon_204@hotmail.com>
2017-10-08 17:43:28 -0700

    GetCompetitivePricingForSKU.php Activated ServiceUrl, Deleted comment: Mock Service, Setup request parameter

--- MarketplaceWebServiceProducts/Functions/GetCompetitivePricingForSKU.php
+++ MarketplaceWebServiceProducts/Functions/GetCompetitivePricingForSKU.php
@@ -18,7 +18,7 @@
  */
 
 /**
- * Get Competitive Pricing For SKU Sample
+ * Get Competitive Pricing For SKU 
  */
 
 require_once('.config.inc.php');
@@ -32,14 +32,7 @@ require_once('.config.inc.php');
  ***********************************************************************/
 // More endpoints are listed in the MWS Developer Guide
 // North America:
-//$serviceUrl = "https://mws.amazonservices.com/Products/2011-10-01";
-// Europe
-//$serviceUrl = "https://mws-eu.amazonservices.com/Products/2011-10-01";
-// Japan
-//$serviceUrl = "https://mws.amazonservices.jp/Products/2011-10-01";
-// China
-//$serviceUrl = "https://mws.amazonservices.com.cn/Products/2011-10-01";
-
+$serviceUrl = "https://mws.amazonservices.com/Products/2011-10-01";
 
  $config = array (
    'ServiceURL' => $serviceUrl,
@@ -57,30 +50,13 @@ require_once('.config.inc.php');
         APPLICATION_VERSION,
         $config);
 
-/************************************************************************
- * Uncomment to try out Mock Service that simulates MarketplaceWebServiceProducts
- * responses without calling MarketplaceWebServiceProducts service.
- *
- * Responses are loaded from local XML files. You can tweak XML files to
- * experiment with various outputs during development
- *
- * XML files available under MarketplaceWebServiceProducts/Mock tree
- *
- ***********************************************************************/
- // $service = new MarketplaceWebServiceProducts_Mock();
-
-/************************************************************************
- * Setup request parameters and uncomment invoke to try out
- * sample for Get Competitive Pricing For SKU Action
- ***********************************************************************/
- // @TODO: set request. Action can be passed as MarketplaceWebServiceProducts_Model_GetCompetitivePricingForSKU
  $request = new MarketplaceWebServiceProducts_Model_GetCompetitivePricingForSKURequest();
  $request->setSellerId(MERCHANT_ID);
  // object or array of parameters
  invokeGetCompetitivePricingForSKU($service, $request);
 
 /**
-  * Get Get Competitive Pricing For SKU Action Sample
+  * Get Competitive Pricing For SKU Action 
   * Gets competitive pricing and related information for a product identified by
   * the MarketplaceId and ASIN.
   *
@@ -92,10 +68,7 @@ require_once('.config.inc.php');
   {
       try {
         $response = $service->GetCompetitivePricingForSKU($request);
-
-        echo ("Service Response\n");
-        echo ("=============================================================================\n");
-
+     
         $dom = new DOMDocument();
         $dom->loadXML($response->toXML());
         $dom->preserveWhiteSpace = false;
