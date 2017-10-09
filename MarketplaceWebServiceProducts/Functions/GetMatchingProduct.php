a81c62fd7b89b91719a9ffc6c217403a62a58042
LeonUu <leon_204@hotmail.com>
2017-10-08 18:01:53 -0700

    GetMatchingProduct.php Added description, Activated ServiceUrl, Deleted comment: Mock Service, Setup request sample

--- MarketplaceWebServiceProducts/Functions/GetMatchingProduct.php
+++ MarketplaceWebServiceProducts/Functions/GetMatchingProduct.php
@@ -18,7 +18,7 @@
  */
 
 /**
- * Get Matching Product Sample
+ * Get Matching Product 
  */
 
 require_once('.config.inc.php');
@@ -32,14 +32,7 @@ require_once('.config.inc.php');
  ***********************************************************************/
 // More endpoints are listed in the MWS Developer Guide
 // North America:
-//$serviceUrl = "https://mws.amazonservices.com/Products/2011-10-01";
-// Europe
-//$serviceUrl = "https://mws-eu.amazonservices.com/Products/2011-10-01";
-// Japan
-//$serviceUrl = "https://mws.amazonservices.jp/Products/2011-10-01";
-// China
-//$serviceUrl = "https://mws.amazonservices.com.cn/Products/2011-10-01";
-
+$serviceUrl = "https://mws.amazonservices.com/Products/2011-10-01";
 
  $config = array (
    'ServiceURL' => $serviceUrl,
@@ -57,32 +50,14 @@ require_once('.config.inc.php');
         APPLICATION_VERSION,
         $config);
 
-/************************************************************************
- * Uncomment to try out Mock Service that simulates MarketplaceWebServiceProducts
- * responses without calling MarketplaceWebServiceProducts service.
- *
- * Responses are loaded from local XML files. You can tweak XML files to
- * experiment with various outputs during development
- *
- * XML files available under MarketplaceWebServiceProducts/Mock tree
- *
- ***********************************************************************/
- // $service = new MarketplaceWebServiceProducts_Mock();
-
-/************************************************************************
- * Setup request parameters and uncomment invoke to try out
- * sample for Get Matching Product Action
- ***********************************************************************/
- // @TODO: set request. Action can be passed as MarketplaceWebServiceProducts_Model_GetMatchingProduct
  $request = new MarketplaceWebServiceProducts_Model_GetMatchingProductRequest();
  $request->setSellerId(MERCHANT_ID);
  // object or array of parameters
  invokeGetMatchingProduct($service, $request);
 
 /**
-  * Get Get Matching Product Action Sample
-  * Gets competitive pricing and related information for a product identified by
-  * the MarketplaceId and ASIN.
+  * Get Matching Product Action 
+  * Returns a list of products and their attributes, based on specified ASIN.
   *
   * @param MarketplaceWebServiceProducts_Interface $service instance of MarketplaceWebServiceProducts_Interface
   * @param mixed $request MarketplaceWebServiceProducts_Model_GetMatchingProduct or array of parameters
