fe928d265572d9cb6819dfa82f6c2a1185dab2c9
LeonUu <leon_204@hotmail.com>
2017-10-15 15:47:00 -0700

    removed echo

--- MarketplaceWebServiceProducts/Functions/GetMatchingProduct.php
+++ MarketplaceWebServiceProducts/Functions/GetMatchingProduct.php
@@ -68,9 +68,6 @@ $serviceUrl = "https://mws.amazonservices.com/Products/2011-10-01";
       try {
         $response = $service->GetMatchingProduct($request);
 
-        echo ("Service Response\n");
-        echo ("=============================================================================\n");
-
         $dom = new DOMDocument();
         $dom->loadXML($response->toXML());
         $dom->preserveWhiteSpace = false;
