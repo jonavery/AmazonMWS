2e74464531adf57492e6fbe563e46db2d1245974
LeonUu <leon_204@hotmail.com>
2017-10-22 12:17:28 -0700

    added syntax

--- MarketplaceWebServiceProducts/Functions/CreateArray.php
+++ MarketplaceWebServiceProducts/Functions/CreateArray.php
@@ -3,7 +3,7 @@
 /***************************************************************
 * CreateArray.php queries the database and creates
 * an array containing the items ready for listing
-* based on the input small items, palleted items or electronics. 
+* based on the input small items, palleted items or electronics.
 *
 * The script then converts that array into an xml filee that
 * can be fed to Amazon via the script "SubmitFeed"
@@ -19,7 +19,7 @@ require_once(__DIR__ . '/GetLowestOfferListingsForASIN.php');
 $requestPrice = $request;
 unset($request);
 
-// Get the list 
+// Get the list
 require_once(__DIR__ . '/ListMatchingProducts.php');
 $requestMatch = $request;
 unset ($request);
@@ -28,7 +28,7 @@ unset ($request);
 
 $type = htmlspecialchars($_GET["type"]);
 	Switch($type) {
-	case 'e': 
+	case 'e':
 		$url = "https://script.google.com/macros/s/AKfycbx8EuLlIqz8EPVXYV0kHDfgxLxUsoNL_4cZhtHlvcC0bl7IQG0/exec";
 		break;
 	case 'sp':
@@ -42,7 +42,7 @@ $type = htmlspecialchars($_GET["type"]);
 		exit;
 }
 
-// Parse the data from XML file into an array
+// Parse data from XML into an array
 $itemsXML = file_get_contents($url);
 $items = new SimpleXMLElement($itemsXML);
 $itemArray = array();
@@ -61,42 +61,40 @@ foreach ($items->item as $item) {
 			"Weight"=>(string)$item->Dimensions->Weight,
 			"Length"=>(string)$item->Dimensions->Length,
 			"Width"=>(string)$item->Dimensions->Width,
-			"Height"=>(string)$item->Dimensions->Height));		
+			"Height"=>(string)$item->Dimensions->Height));
 }
-// Create an array to hold UPC's
-$upclist = array();
+// Create an array to hold UPC's.
+$upcList = array();
 
 // Cache throttling parameter
 $reqquestcount = 0;
 
 // Pass item array through for loop and format UPC
-echo "Creating array....\n";
-foreach($itemArray as $key => $item) {
-	switch(strlen($item["UPC"])) {
-	case 11:
-		$item["UPC"] = "0".$item["UPC"];
-		break;
+echo "Creating array... \n";
+foreach($itemArray as $key => &$item) {
+    switch(strlen($item["UPC"])) {
+        case 11:
+            $item["UPC"] = "0".$item["UPC"];
+            break;
 	case 12:
-		break;
-	case 10:
-		$item["ASIN"] = $item["UPC"];
-		continue 2;
-	default:
-		$requestMatch->setQuery($item["Title"]);
-		$xmlMatch = invokeListMatchingProducts($service, $requestMatch);
-		$match = new SimpleXMLElement($xmlMatch);
-		$item["ASIN"] = (string)$match->ListMatchingProductsResult->Products->Product->Identifiers->MarketplaceASIN->ASIN;
-		
-		// Sleep for required time to avoid throttling.
-		$match_end = microtime(true);
-		if ($requestCount > 19 && ($match_end - $match_start) < 5000000) {
-			usleep(5000000 - ($match_end - match_start));
-		}
-		$match_start = microtime(true);
-		continue 2;
-			
-	}
-	$requestCount ++;
+            break;
+        case 10:
+            $item["ASIN"] = $item["UPC"];
+            continue 2;
+        default:
+            $requestMatch->setQuery($item["Title"]);
+            $xmlMatch = invokeListMatchingProducts($service, $requestMatch);
+            $match = new SimpleXMLElement($xmlMatch);
+            $item["ASIN"] = (string)$match->ListMatchingProductsResult->Products->Product->Identifiers->MarketplaceASIN->ASIN;
+            // Sleep for required time to avoid throttling.
+    	    $match_end = microtime(true);
+    	    if ($requestCount > 19 && ($match_end - $match_start) < 5000000) {
+       	        usleep(5000000 - ($match_end - $match_start));
+    	    }
+    	    $match_start = microtime(true);
+            continue 2;
+    }
+    $requestCount++;
 
 // Set the ID and ID type to be converted to an ASIN.
 	$requestID->setIdType('UPC');
@@ -107,30 +105,31 @@ foreach($itemArray as $key => $item) {
 	$xmlid = invokeGetMatchingProductForId($service, $requestId);
 
 // Parse the XML response and add ASINS to item array.
-	$asins = new SImpleXMLElement($xmlID);
+	$asins = new SimpleXMLElement($xmlID);
 	$result = $asins->GetMatchingProductforIdResult;
 	if (@count($result->Products)) {
-		$product = $result->Products->product->children();
+		$product = $result->Products->Product->children();
 		$item["ASIN"] = (string)$product->Identifiers->MarketplaceASIN->ASIN;
 	}
 
 // Sleep for required time to avoid throttling.
 	$time_end = microtime(true);
-	if (requestCount > 19 && ($time_end - $time_start) < 200000) {	
+	if ($requestCount > 19 && ($time_end - $time_start) < 200000) {
 		usleep(200000 - ($time_end - $time_start));
 	}
 	$time_start = microtime(true);
 }
-	Echo "Generatimg prices...\n";
-	$itemArry = parseOffers($itemArray, $requestPrice);
+	echo "Generating prices... \n";
+	$itemArray = parseOffers($itemArray, $requestPrice);
+	print_r($itemArray);
 
 		$itemJSON = json_encode($itemArray);
 		file_put_contents("MWS.json", $itemJSON);
 
-// Based on the selection input (e, sp, o) show the following message	
+// Based on the selection input (e, sp, o) show the following message
  $case = htmlspecialchars($_GET["type"]);
 	Switch($type) {
-	case 'e': 
+	case 'e':
 		echo "Success! Electronics MWS.json has been created. Run 'Populate MWS Tab' and 'Post Listings' to list products on Amazon.";
 		break;
 	case 'sp':
